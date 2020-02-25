f07c3296c0f7c7a74cfd148a72d9e649a70f8aa7
Max <LentyaiPrOO@yandex.ru>
2020-02-25 21:28:42 +0300

    basic functional

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Row,
+  Container,
+  Col,
+  InputGroup,
+  FormControl,
+  Button,
+  Alert
+} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import styles from "./App.module.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      notes: [],
+      inputText: ""
+    };
+  }
+
+  AddNote = () => {
+    this.setState(state => {
+      return {
+        notes: [
+          ...state.notes,
+          { id: Date.now().toString(), text: state.inputText }
+        ],
+        inputText: ""
+      };
+    });
+  };
+
+  DeleteNote = id => {
+    this.setState(state => {
+      let tmpState = [...state.notes];
+      return {
+        notes: tmpState.filter(item => {
+          return item.id !== id;
+        })
+      };
+    });
+  };
+
+  render() {
+    return (
+      <Container>
+        <Row className={styles.mbt20}>
+          <Col>
+            <h3 className="text-center">Todo App</h3>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <InputGroup className="mb-3">
+              <FormControl
+                placeholder="Введите текст задачи"
+                aria-label="Введите текст задачи"
+                value={this.state.inputText}
+                onChange={e => this.setState({ inputText: e.target.value })}
+              />
+              <InputGroup.Append>
+                <Button variant="primary" onClick={this.AddNote}>
+                  Добавить
+                </Button>
+              </InputGroup.Append>
+            </InputGroup>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            {this.state.notes.map(item => (
+              <Note
+                key={item.id}
+                text={item.text}
+                onDelete={() => this.DeleteNote(item.id)}
+              />
+            ))}
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
-export default App;
+class Note extends React.Component {
+  render() {
+    return (
+      <Alert variant="primary">
+        {this.props.text}
+        <hr />
+        <Alert.Link onClick={() => this.props.onDelete()}>
+          Удалить задачу
+        </Alert.Link>
+      </Alert>
+    );
+  }
+}
